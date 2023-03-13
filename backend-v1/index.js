4aad88444ea08010f80007044457c56f323214b0
Kabir Bissessar <kabirrbissessar@gmail.com>
2023-03-13 02:10:20 +0200

    removed unnecessary lines

--- backend-v1/index.js
+++ backend-v1/index.js
@@ -1,32 +1,18 @@
-// import firebase from "./config"
-
 require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
 
-// firebase config
-// const { initializeApp } = require("firebase-admin/app")
+// Firebase config
 const admin = require("firebase-admin");
 const creds = require("./serviceKey.json")
 const { getFirestore } = require('firebase-admin/firestore');
 
-// const firebaseConfig = {
-//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-// };
 
-// Initialize Firebase
-// const firebaseApp = admin.initializeApp(firebaseConfig);
+// Initialize Firebase in admin mode
 const firebaseApp = admin.initializeApp({
     credential: admin.credential.cert(creds)
 });
-
-// const firebase = require("./Firebase/firebaseApp");
 
 const app = express();
 
@@ -36,9 +22,8 @@ app.use(express.json());
 const db = getFirestore();
 const usersRef = db.collection('users');
 
-app.get("/", async (req, res) => {
+app.get("/api", async (req, res) => {
     const snapshot= await usersRef.get();
-    // const snapshot = await getDocs(collection(db, 'users'))
     const userData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
     console.log(userData)
     res.send(userData)
@@ -46,12 +31,11 @@ app.get("/", async (req, res) => {
 
 app.post("/api/create", async (req, res) => {
     const data = req.body;
-    console.log("User added with ID ", data.id);
-    delete data.id
     const newUser = await usersRef.add({ data });
-    // const newUser = await setDoc(doc(db, 'users', data.id), data )
+    console.log("User added with ID ", newUser.id);
     res.send({ msg: "User added"});
 })
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
